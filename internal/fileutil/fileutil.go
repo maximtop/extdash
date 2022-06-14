29c9d513f2dc2277c6faf07958f072c1a6de89fb
Maxim Topciu <mtopciu@adguard.com>
2022-06-14 22:02:00 +0300

    use io instead of ioutil

--- internal/fileutil/fileutil.go
+++ internal/fileutil/fileutil.go
@@ -3,7 +3,7 @@ package fileutil
 import (
 	"archive/zip"
 	"errors"
-	"io/ioutil"
+	"io"
 )
 
 const (
@@ -19,7 +19,7 @@ func readFile(file *zip.File) (result []byte, err error) {
 		return nil, err
 	}
 	defer reader.Close()
-	content, err := ioutil.ReadAll(reader)
+	content, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, err
 	}
